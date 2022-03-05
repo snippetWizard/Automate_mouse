bc7ea8676bedd82bc7914c5ee5fb557b23bd915b
Sagar-codes <paulsagar5433@gmail.com>
2022-03-05 20:20:50 +0530

    first commit

---
+++ Mouse_automate/mouse.py
@@ -0,0 +1,23 @@
+# import pyautogui
+
+# print(pyautogui.position()) # current mouse x and y
+# print(pyautogui.size()) # current screen resolution width and height
+
+
+# pyautogui.moveTo(600, 500, duration=2)  # move mouse to XY coordinates over 2 seconds
+# pyautogui.moveRel(200, 0, duration=2)  # move mouse relative to its current position
+
+# pyautogui.dragTo(350, 250, duration=2)  # drag mouse to XY
+# pyautogui.dragRel(200, 0, duration=) # drag mouse relative to its current position
+
+# pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
+
+# pyautogui.rightClick(x=moveToX, y=moveToY)
+# pyautogui.middleClick(x=moveToX, y=moveToY)
+# pyautogui.doubleClick(x=moveToX, y=moveToY)
+# pyautogui.tripleClick(x=moveToX, y=moveToY)
+
+# pyautogui.scroll(amount_to_scroll, x=moveToX, y=moveToY)
+
+# pyautogui.mouseDown(x=moveToX, y=moveToY, button='left')
+# pyautogui.mouseUp(x=moveToX, y=moveToY, button='left')
---
+++ instruction.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+
+print('File is Running')
+
+pyautogui.moveTo(72, 593, duration=1)
+pyautogui.click()
+pyautogui.sleep(0.5)
+pyautogui.moveTo(76, 674, duration=2)
+pyautogui.click()
